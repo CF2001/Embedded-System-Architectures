a5e8c560776595cd7837b97d0f13d7dbeb1cc88a
Ana Rosa <a.claudia@ua.pt>
2023-05-16 11:47:40 +0100

    update distance formula

--- main/measure_dist.c
+++ main/measure_dist.c
@@ -4,22 +4,27 @@
 #include "freertos/task.h"
 #include "freertos/event_groups.h"
 
-#include "esp_timer.h"
-#include <esp32/rom/ets_sys.h>
+#include "esp_timer.h"      // High Resolution Timer (ESP Timer)
+//#include <esp32/rom/ets_sys.h>
+#include <rom/ets_sys.h>    // Note that this is busy-waiting – it does not allow other tasks to run, it just burns CPU cycles.
 
 #include "wifi_station.h"
+
+static const char *TAG =  "MEASURES";
 
 #define IN_GPIO 14
 #define OUT_GPIO 25
 #define TRIGGER_PIN OUT_GPIO    // envia o sinal de 10 us para o trigger 
 #define ECHO_PIN IN_GPIO        // recebe a resposta de tempo do echo 
 
-#define TRIGGER_LOW_DELAY 4     // 4us = 0.004 ms 
+#define TRIGGER_LOW_DELAY 2     // 2us
 #define TRIGGER_HIGH_DELAY 10   // 10us
 
 #define ROUNDTRIP_M 5800.0f
 
-float distance;
+float distance = 0;
+float speedOfSound = 0;
+float temperature = 20;
 
 
 static void configure_pins(void)
@@ -36,14 +41,18 @@ static void configure_pins(void)
 
 static void ultrasonic_measure(void *pvParameters)
 {
+    speedOfSound = (((331.5 + (0.6 * temperature)) * 100) / 1000000); // m/s = m/s * 100 (cm/s) = cm/s / 1 000 000 (cm/us)
+    //ESP_LOGI(TAG, "Speed of Sound: %f", speedOfSound);
+
     while(true)
     {
+        // Clears the trigger Pin
+        gpio_set_level(TRIGGER_PIN, 0); 
+        ets_delay_us(TRIGGER_LOW_DELAY);    // delay 2us    
+
         // Send a trigger pulse (10us) to the HC-SR04P
-        gpio_set_level(TRIGGER_PIN, 0); // low = 4 us
-        //vTaskDelay(pdMS_TO_TICKS((uint32_t) TRIGGER_LOW_DELAY ));  // ms -> tick (1tick = 10 ms)  ; 4us = 0.004 ms
-        ets_delay_us(TRIGGER_LOW_DELAY);
         gpio_set_level(TRIGGER_PIN, 1);
-        ets_delay_us(TRIGGER_HIGH_DELAY);
+        ets_delay_us(TRIGGER_HIGH_DELAY);   // delay 10us
         gpio_set_level(TRIGGER_PIN, 0);
 
         // Wait for the echo signal
@@ -51,16 +60,21 @@ static void ultrasonic_measure(void *pvParameters)
         while (!gpio_get_level(ECHO_PIN));   // enquato estiver high 
 
         // got echo, measuring
-        int64_t echo_start = esp_timer_get_time();
+        int64_t echo_start = esp_timer_get_time();  // Get time in microseconds since boot.
         int64_t time = echo_start;
-        while (gpio_get_level(ECHO_PIN))
+        while (gpio_get_level(ECHO_PIN))    // while is high
         {
-            time = esp_timer_get_time();
+            time = esp_timer_get_time();    
         }
 
-        uint32_t time_us;
-        time_us = time - echo_start;
-        distance = (time_us / ROUNDTRIP_M) * 100;
+        uint32_t time_echo_isHigh;
+        time_echo_isHigh = time - echo_start;   // us (microsec)
+
+        ESP_LOGI(TAG, "Speed of Sound: %f", speedOfSound);
+        ESP_LOGI(TAG, "Time echo is High: %ld", time_echo_isHigh);
+        distance = ((speedOfSound * time_echo_isHigh) / 2); 
+
+        //distance = (time_us / ROUNDTRIP_M) * 100;
         
         printf("Distance: %0.04f cm\n", distance);
 
